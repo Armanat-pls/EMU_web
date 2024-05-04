bfd64292c6b51d08109a1b72d3b26b59abd2070d
RedchenkoDenis <dr.armanat@gmail.com>
2024-05-04 16:42:20 +0300

    config cleanup

--- src/main/java/emulator/core/Config.java
+++ src/main/java/emulator/core/Config.java
@@ -5,5 +5,4 @@ public class Config  {
 	public static final int MEM = 256; // Количество ячеек памяти
 	public static final int BMEM = 8; // Количество бит на адрес памяти
 	public static final String VER = "2.0 Web";
-	public static int MEMzeros = (int)Math.ceil(Math.log10(MEM + 1));
 }
