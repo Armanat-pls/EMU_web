84c4a0e2a2d92f5de7bfd29f94a3017764f09ab5
RedchenkoDenis <dr.armanat@gmail.com>
2024-05-04 16:38:30 +0300

    MEMORY class cleanup

--- src/main/java/emulator/core/MEMORY.java
+++ src/main/java/emulator/core/MEMORY.java
@@ -3,7 +3,7 @@ package emulator.core;
 import java.util.BitSet;
 import static emulator.core.Config.*;
 
-public class  MEMORY{	//память
+public class MEMORY{	//память
 
 	private BitSet[] TABLE; //массив ячеек памяти
 
@@ -22,37 +22,5 @@ public class  MEMORY{	//память
 	public void zero(){
 		for (int i = 0; i < MEM; i++)
 			this.TABLE[i].clear();
-	}
-	public String show_cell(int addr){  
-		return show_bitset(this.TABLE[addr]);
-	}
-
-	//обработка строки и ввод в память
-	public boolean file_RAMfill(int addr, String line){
-		BitSet writeline = new BitSet(CELL);
-		boolean success = true;
-		line = line.replace(" ", "");
-		if (line.length() < CELL) //удлиннение строки при необходимости
-		{
-			String temp = "";
-			for (int i = 0; i < CELL - line.length(); i++)
-				temp += "0";
-			line = temp + line;
-		}
-		for (int i = 0; i < CELL; i++)
-		{
-			if (line.charAt(i) == '0')
-				writeline.clear(CELL - 1 - i);
-			else if (line.charAt(i) == '1')
-				writeline.set(CELL - 1 - i);
-			else 
-			{
-				writeline.clear();
-				success = false;
-			}
-		}
-		if (success)
-			this.write_cell(addr, writeline);
-		return success;
 	}
 }
